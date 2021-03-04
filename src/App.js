eebb906c3199f7bf022f6fbcaac062131a17b493
aveekSF <53829299+aveekSF@users.noreply.github.com>
2021-03-04 13:19:31 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import './App.css';
+import SearchPhotos from "./searchPhotos"
+
+function App() {
+  return (
+    <div className="App">
+     
+ <div className="container">
+        <SearchPhotos /> 
+        </div>
+    </div>
+  );
+}
+export default App;
